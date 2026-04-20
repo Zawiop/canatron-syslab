7b275bae1de79d36714091279174f31409fcfa63
Arrush Shah <shaharrush@gmail.com>
2026-04-20 19:04:24 -0400

    2m model

--- MYMODEL.py
+++ MYMODEL.py
@@ -249,7 +249,7 @@ if __name__ == "__main__":
         save_path="./checkpoints/",
         name_prefix="catan",
     )
-    hours = 1
+    hours = 4   
     timesteps = 5000000*hours
     try:
         model.learn(total_timesteps=timesteps, callback=checkpoint_cb)
